1fbae5d3d14a3e6a5cbc9b10dde97df7c0613c81
smkumatela <smkumatela@webmail.co.za>
2016-03-05 16:22:02 +0200

    first commit

---
+++ Documents/Brain Wash/Term1 Assignments/Calculating/dividingNum/src/main/java/za/ac/calculate/cput/divideNum.java
@@ -0,0 +1,12 @@
+package za.ac.calculate.cput;
+
+/**
+ * Created by Songezo on 2016-03-05.
+ */
+public class divideNum {
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+}
---
+++ Documents/Brain Wash/Term1 Assignments/LabExce/src/main/java/za/ac/mkumsPrg/Integers.java
@@ -0,0 +1,7 @@
+package za.ac.mkumsPrg;
+
+/**
+ * Created by Songezo on 2016-03-01.
+ */
+public class Integers {
+}
